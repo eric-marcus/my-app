4f63d7d5256cebae76ddf9a262658378b71c42e0
Eric <48383124+eric-marcus@users.noreply.github.com>
2022-07-12 07:56:08 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function Form (props) {
+  const [userName, setUserName] = useState();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('do submit')
+    fetchData();
+  };
+  const fetchData = () => {
+    fetch(`https://api.github.com/users/${userName}`).then(resp => {
+      resp.json().then(data => {
+        console.log(data);
+        props.setData(data);
+      })
+    })
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={userName}
+          onChange={event => {
+            console.log(event.target.value);
+            setUserName(event.target.value)}}
+          placeholde='github username'
+          required
+        />
+        <button>Add card</button>
+      </form>
+    </div>
+  );
+}
+
+
+
+function CardList (props) {
+  console.log(props.data);
+  return(
+    <div>
+      {/* <Card {...props}/> */}
+      {props.data !== null ? props.data.map(item => <Card {...item}/>) : 'null'}
+    </div>
+  )
+}
+
+function Card (props) {
+  const data = props;
+  return (
+    <div>
+      <p>sadf</p>
+      <img src={data.avatar_url}></img>
+      {/* <img src='https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'></img> */}
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+        <div style={{fontSize: '125%'}}>{data.name}</div>
+        <div>{data.company}</div>
+      </div>
+    </div>
+    
+  )
+}
+
+function App (props) {
+  const [data, setData] = useState([]);
+  return (
+    <div>
+      <Form setData={setData}/>
+      <CardList data={data}/>
+    </div>
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App title='github profile cards'/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
